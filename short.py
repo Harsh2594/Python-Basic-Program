5066602f8f19838c4758fba229b4564768da5677
Harsh2594 <harsh.sgm13@gmail.com>
2024-08-06 19:07:29 +0530

    Reverse the given list

--- short.py
+++ short.py
@@ -1,6 +1,7 @@
 lst = input("Enter intiger value seperated by space").split()
 my_list = [int(s) for s in lst]
 n = len(my_list)
+#Short list in increasing order
 for i in range(n):
   for j in range(n-i-1):
     if my_list[j]>my_list[j+1]:
@@ -9,6 +10,11 @@ for i in range(n):
       my_list[j] = my_list[j]-my_list[j+1]
 
 print(my_list)
+#Reverse the given list
+my_list2 = []
+for i in range(n,0,-1):
+  my_list2 = my_list2.append(my_list[i-1])
+print(my_list2)  
 
    
 
