eb9c1511f7336b07d3906625dd11d54e2aac03ea
Harsh2594 <harsh.sgm13@gmail.com>
2024-08-06 19:13:30 +0530

    Find second largest No.

--- short.py
+++ short.py
@@ -10,12 +10,17 @@ for i in range(n):
       my_list[j] = my_list[j]-my_list[j+1]
 
 print(my_list)
+
+
 #Reverse the given list
 my_list2 = []
 for i in range(n,0,-1):
   my_list2 = my_list2.append(my_list[i-1])
 print(my_list2)  
 
+
+#Find Second largest number 
+print(my_list[-2])
    
 
 
