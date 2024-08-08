0320c3027209418ea5d5f83e2ff608347bbc457e
Harsh2594 <harsh.sgm13@gmail.com>
2024-08-08 19:53:50 +0530

    Factorial using recursion

--- factorial.py
+++ factorial.py
@@ -4,3 +4,15 @@ for i in range(num):
     fact = fact*num
     num -= 1
 print(fact)    
+
+#Factorial using recursion:
+
+n = int(input("Enter number: "))
+
+def factorial(n):
+    if n == 1:
+        return n
+    return factorial(n-1)*n
+    
+print(factorial(n))    
+        
