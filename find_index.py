9946436e1a05f38477254b6bac0256189405a6bc
Harsh2594 <harsh.sgm13@gmail.com>
2024-08-14 09:17:33 +0530

    find index

---
+++ find_index.py
@@ -0,0 +1,25 @@
+#find index of two list element whose sum is given:
+l = list(map(int,input("Enter the number separated by space").split()))
+n = len(l)
+sum = int(input("Enter the sum"))
+result = []
+for i in range(n):
+    for j in range(i+1,n-i):
+        print(i,j)
+        if l[i]+l[j] == sum:
+            result.append(i)
+            result.append(j)
+            break
+    
+print(result)        
+        
+            
+        
+
+
+
+
+    
+                
+
+    
